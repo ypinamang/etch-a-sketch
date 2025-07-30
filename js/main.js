f9450247fde42273acaf58ca7acd505caaced2fd
Yaw <ypinamangyapong@gmail.com>
2025-07-30 19:12:01 +0900

    Constrain sketch area to fixed size

--- js/main.js
+++ js/main.js
@@ -1,27 +1,61 @@
-// Create a 4x4 grid
 const mainWrapper = document.querySelector(".main-wrapper");
-const sizeButton = document.querySelector(".size-btn");
+const createButton = document.querySelector(".size-btn");
+const clearButton = document.querySelector(".clear-btn");
+const colorPicker = [""]
+let gridSize = 12
 
-let gridSize = 12;
+createButton.addEventListener("click", () =>
+{
+
+    gridSize = parseInt(prompt("What size square grid do you have in mind?"));
+    if (!isNaN(gridSize) && (gridSize > 0) && (gridSize <= 100)){
+        createGrid(gridSize);
+    } else {
+        alert("Enter a number between 1 and 100")
+}
+}
+);
 
 
+clearButton.addEventListener("click", () => {
+    clearGrid();
+    createGrid(gridSize);
+});
 
-for (let i = 1; i <= gridSize; i++) {
+function createGrid(size=gridSize) {
+    clearGrid();
+    for (let i = 1; i <= size; i++) {
     const singleRow = document.createElement("div");
     singleRow.classList.add("single-row");
-    for (let j = 1; j<= gridSize; j++) {
+    for (let j = 1; j <= size; j++) {
         const smallSquare = document.createElement("div");
         smallSquare.classList.add("small-square");
         singleRow.appendChild(smallSquare);
     }
     mainWrapper.appendChild(singleRow);
+    }
+
+    const smallSquares = document.querySelectorAll(".small-square");
+    smallSquares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+        const squareStyle = getComputedStyle(square);
+        const currentOpacity = parseFloat(squareStyle.opacity);
+        const opacityIncrement = currentOpacity + 0.3;
+        square.style.opacity = opacityIncrement.toString();
+    });
+});
+
 }
 
-const gridSquares = document.querySelectorAll(".small-square");
+function clearGrid() {
+    let wrapperChild = mainWrapper.lastElementChild;
+    while(wrapperChild) {
+        wrapperChild.remove();
+        wrapperChild = mainWrapper.lastElementChild;
+    }
+}
 
-gridSquares.forEach((gridSquare) => {
-    gridSquare.addEventListener("mouseover", () => {
-        gridSquare.style.backgroundColor = "black";
-        gridSquare.style.opacity = "1";
-    })
-});
+
+createGrid();
+
+
